fe4f077610896c4a3a9618f9d218bd82e2389ef3
shaazshaikh <shaazshaikh111@gmail.com>
2024-10-04 18:02:56 +0530

    Add project files.

---
+++ FileSystemDatabaseProject/dbo/Tables/Files.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Files] (
+    [Id]              UNIQUEIDENTIFIER NOT NULL,
+    [UserId]          UNIQUEIDENTIFIER NOT NULL,
+    [FileName]        NVARCHAR (50)    NOT NULL,
+    [FileSize]        BIGINT           NOT NULL,
+    [StoragePath]     NVARCHAR (MAX)   NOT NULL,
+    [ParentFolderId]  UNIQUEIDENTIFIER NULL,
+    [FilePath]        NVARCHAR (MAX)   NOT NULL,
+    [IsDeleted]       BIT              DEFAULT ((0)) NOT NULL,
+    [IsDeletedByUser] BIT              DEFAULT ((0)) NOT NULL,
+    [CreatedDate]     DATETIME         NOT NULL,
+    [ModifiedDate]    DATETIME         NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ FileSystemDatabaseProject/dbo/Tables/Folders.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Folders] (
+    [Id]              UNIQUEIDENTIFIER NOT NULL,
+    [UserId]          UNIQUEIDENTIFIER NOT NULL,
+    [FolderName]      NVARCHAR (50)    NOT NULL,
+    [FolderSize]      BIGINT           NULL,
+    [ParentFolderId]  UNIQUEIDENTIFIER NULL,
+    [FolderPath]      NVARCHAR (MAX)   NOT NULL,
+    [IsDeleted]       BIT              DEFAULT ((0)) NOT NULL,
+    [IsDeletedByUser] BIT              DEFAULT ((0)) NOT NULL,
+    [CreatedDate]     DATETIME         NOT NULL,
+    [ModifiedDate]    DATETIME         NOT NULL,
+    [StoragePath]     NVARCHAR (MAX)   NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
